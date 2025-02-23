21526592be70f7fcbf27255e2033afedab6dde3e
Kenneth Tannery <Scykt6@nottingham.edu.cn>
2025-02-23 17:51:41 +0800

    3d commit

--- routes/auth.js
+++ routes/auth.js
@@ -6,8 +6,8 @@ const bcrypt = require('bcrypt');
 router.post("/register", async (req, res) => {
     try {
         //generate hashed password
-        const salt = await brcypt.genSalt(10);
-        const hashedPassword = await brcypt.hash(req.body.password, salt);
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);        
 
         //generate new user
         const newUser = new User({
@@ -21,6 +21,26 @@ router.post("/register", async (req, res) => {
         res.status(200).json(user);
     } catch (err) {
         console.log(err);
+    }
+});
+
+//login
+router.post("/login", async (req, res) => {
+    try {
+        const user = await User.findOne({ email: req.body.email });
+        if (!user) {
+            return res.status(404).json("user not found");
+        }
+
+        const validPassword = await bcrypt.compare(req.body.password, user.password);
+        if (!validPassword) {
+            return res.status(400).json("Wrong password");
+        }
+
+        res.status(200).json(user);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ error: "Internal server error" });
     }
 });
 
