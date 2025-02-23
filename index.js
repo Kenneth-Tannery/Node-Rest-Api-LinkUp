0980aa484db1ef87206d160898a4509e210c0210
Kenneth Tannery <Scykt6@nottingham.edu.cn>
2025-02-23 14:14:34 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const userRoute = require('./routes/users');
+const authRoute = require('./routes/auth');
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('MongoDB connection error:', err));
+
+// Middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan('common'));
+
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+
+app.listen(8800, () => {
+    console.log('Server started on http://localhost:8800');
+});
